3e150053d0f2ee9e50add54b21c72ca6cc469f1f
SolidAlloy <artem.perepelytsia@4a-games.com.mt>
2020-10-13 14:10:44 +0300

    Added the a scriptable object container for the window fields

--- Editor/TypeSelectionWindow.cs
+++ Editor/TypeSelectionWindow.cs
@@ -62,7 +62,7 @@
             var windowPos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
             position = new Rect(windowPos, position.size);
 
-            var dropdownDrawer = new TypeDropdownDrawer(null, new TypeOptionsAttribute { ExcludeNone = true }, null);
+            var dropdownDrawer = new TypeDropdownDrawer(null, new TypeOptionsAttribute { ExcludeNone = true, SerializableOnly = true }, null);
             dropdownDrawer.Draw(type => _onTypeSelected(new[] { type }));
 
             _guiWasSetUp = true;
@@ -72,16 +72,29 @@
     public class MultipleTypeSelectionWindow : TypeSelectionWindow
     {
         private Action<Type[]> _onTypesSelected;
+        private TypeFieldsContainer _typeFieldsContainer;
+        private SerializedObject _containerSO;
 
         protected override void OnCreate(Action<Type[]> onTypesSelected)
         {
             _onTypesSelected = onTypesSelected;
+            _typeFieldsContainer = CreateInstance<TypeFieldsContainer>();
+            _containerSO = new SerializedObject(_typeFieldsContainer);
             this.Resize(300f, 300f);
         }
 
         protected override void OnGUI()
         {
-            throw new NotImplementedException();
+            SerializedProperty typesArray = _containerSO.FindProperty(nameof(TypeFieldsContainer.Types));
+            for (int i = 0; i < typesArray.arraySize; i++)
+            {
+                EditorGUILayout.PropertyField(typesArray.GetArrayElementAtIndex(i), true);
+            }
         }
+    }
+
+    internal class TypeFieldsContainer : ScriptableObject
+    {
+        public TypeReference[] Types;
     }
 }
