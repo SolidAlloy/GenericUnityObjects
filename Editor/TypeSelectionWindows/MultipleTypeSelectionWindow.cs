6130eb54347c78ff5163346fbec2b290a700b5ca
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-16 23:37:53 +0300

    Created first prototype of handling new constraints for MultipleTypeSelectionWindow

--- Editor/TypeSelectionWindows/MultipleTypeSelectionWindow.cs
+++ Editor/TypeSelectionWindows/MultipleTypeSelectionWindow.cs
@@ -1,13 +1,17 @@
 ﻿namespace GenericScriptableObjects.Editor.TypeSelectionWindows
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using SolidUtilities.Editor.Extensions;
     using SolidUtilities.Editor.Helpers;
     using TypeReferences;
+    using TypeReferences.Editor.Drawers;
+    using TypeReferences.Editor.Util;
     using UnityEditor;
     using UnityEngine;
     using Util;
+    using TypeCache = TypeReferences.Editor.Util.TypeCache;
 
     /// <summary>
     /// A window that has as many TypeReference fields as needed for the asset creation. The user has to choose all
@@ -15,8 +19,8 @@
     /// </summary>
     internal class MultipleTypeSelectionWindow : TypeSelectionWindow
     {
-        [SerializeField, UseDefaultAssembly(ExcludeNone = true, SerializableOnly = true)]
-        private TypeReference[] _typeRefs;
+        [SerializeField]
+        private ExtendedTypeReference[] _typeRefs;
 
         private Action<Type[]> _onTypesSelected;
         private SerializedObject _serializedObject;
@@ -26,7 +30,7 @@
         {
             int typesCount = genericParamConstraints.Length;
             _onTypesSelected = onTypesSelected;
-            _typeRefs = new TypeReference[typesCount];
+            _typeRefs = new ExtendedTypeReference[typesCount];
             _serializedObject = new SerializedObject(this);
             _contentCache = new ContentCache();
             this.Resize(300f, GetWindowHeight(typesCount));
@@ -63,6 +67,75 @@
             const float spacing = 2f;
             float windowHeight = (oneTypeFieldHeight + spacing) * typeFieldsCount + buttonHeight;
             return windowHeight;
+        }
+    }
+
+    [Serializable]
+    public class ExtendedTypeReference : TypeReference // TODO: think of a better name
+    {
+        // TODO: Maybe replace TypeReferences with type names?
+        public TypeReference[] BaseTypes;
+    }
+
+    [CustomPropertyDrawer(typeof(ExtendedTypeReference))]
+    public sealed class TypeReferencePropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorStyles.popup.CalcHeight(GUIContent.none, 0f);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position = ExcludeLabelFromPositionIfNecessary(position, label);
+            DrawTypeReferenceField(position, property);
+        }
+
+        private static Rect ExcludeLabelFromPositionIfNecessary(Rect position, GUIContent label)
+        {
+            if (label == null || label == GUIContent.none)
+                return position;
+
+            var fieldRectWithoutLabel = EditorGUI.PrefixLabel(position, label);
+            return fieldRectWithoutLabel;
+        }
+
+        private void DrawTypeReferenceField(Rect position, SerializedProperty property)
+        {
+            // TODO: simplify the baseTypes creation process. Perhaps get rid of SerializedTypeReference creation.
+            SerializedProperty baseTypesProperty = property.FindPropertyRelative(nameof(ExtendedTypeReference.BaseTypes));
+            List<Type> baseTypes = new List<Type>(baseTypesProperty.arraySize);
+            for (int i = 0; i < baseTypesProperty.arraySize; i++)
+            {
+                SerializedProperty typeRefProperty = baseTypesProperty.GetArrayElementAtIndex(i);
+                var tempSerializedTypeRef = new SerializedTypeReference(typeRefProperty);
+                var baseType = TypeCache.GetType(tempSerializedTypeRef.TypeNameAndAssembly);
+                baseTypes.Add(baseType);
+            }
+
+            var typeOptionsAttribute = new InheritsAttribute(baseTypes.ToArray()) { ExcludeNone = true, SerializableOnly = true };
+            var serializedTypeRef = new SerializedTypeReference(property);
+
+            var selectedType = TypeCache.GetType(serializedTypeRef.TypeNameAndAssembly);
+
+            if (selectedType != null && ! typeOptionsAttribute.MatchesRequirements(selectedType))
+            {
+                Debug.Log($"{property.name} had the {selectedType} value but the type does not match " +
+                          "constraints set in the attribute, so it was set to null.");
+                selectedType = null;
+                serializedTypeRef.TypeNameAndAssembly = string.Empty;
+            }
+
+            var dropdownDrawer = new TypeDropdownDrawer(selectedType, typeOptionsAttribute, fieldInfo?.DeclaringType);
+
+            var fieldDrawer = new TypeFieldDrawer(
+                serializedTypeRef,
+                position,
+                dropdownDrawer,
+                typeOptionsAttribute.ShortName,
+                typeOptionsAttribute.UseBuiltInNames);
+
+            fieldDrawer.Draw();
         }
     }
 }
