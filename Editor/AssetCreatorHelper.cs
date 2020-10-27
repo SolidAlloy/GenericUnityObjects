3789cba23e241e4912df51a9857be6cc6b3ee357
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-27 13:35:58 +0200

    Started generating short hash instead of full type names

---
+++ Editor/AssetCreatorHelper.cs
@@ -0,0 +1,151 @@
+﻿namespace GenericScriptableObjects.Editor
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using JetBrains.Annotations;
+    using SolidUtilities.Editor.EditorWindows;
+    using SolidUtilities.Editor.Helpers;
+    using SolidUtilities.Extensions;
+    using SolidUtilities.Helpers;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.Assertions;
+
+    internal class AssetCreatorHelper
+    {
+        private readonly Type _genericType;
+        private readonly Type[] _paramTypes;
+        private readonly string[] _paramTypeNamesWithoutAssembly;
+        private readonly string _genericTypeClassSafeName;
+        private readonly string _scriptsPath;
+        private readonly string _namespaceName;
+
+        public AssetCreatorHelper(Type genericType, Type[] paramTypes, string namespaceName, string scriptsPath)
+        {
+            _genericType = genericType;
+            _paramTypes = paramTypes;
+            _scriptsPath = scriptsPath;
+            _namespaceName = namespaceName;
+
+            _paramTypeNamesWithoutAssembly = _paramTypes
+                .Select(type => GetTypeNameWithoutAssembly(type.FullName)).ToArray();
+
+            _genericTypeClassSafeName = GetClassSafeTypeName(_genericType.Name);
+        }
+
+        private string DefaultAssetName => $"New {_genericTypeClassSafeName}.asset";
+
+        public void CreateAsset()
+        {
+            if (GenericSODatabase.ContainsKey(_genericType, _paramTypes))
+            {
+                CreateAssetInteractively();
+                return;
+            }
+
+            Type genericTypeWithArgs = _genericType.MakeGenericType(_paramTypes);
+
+            Type existingAssetType = GetEmptyTypeDerivedFrom(genericTypeWithArgs);
+
+            if (existingAssetType != null)
+            {
+                CreateAssetFromExistingType(existingAssetType);
+                return;
+            }
+
+            AssetCreatorPersistentStorage.SaveForAssemblyReload(genericTypeWithArgs, _namespaceName, _scriptsPath);
+            string className = GetUniqueClassName();
+            CreateScript(className);
+        }
+
+        public void CreateAssetFromExistingType()
+        {
+            Type genericTypeWithArgs = _genericType.MakeGenericType(_paramTypes);
+            Type existingAssetType = GetEmptyTypeDerivedFrom(genericTypeWithArgs);
+            Assert.IsNotNull(existingAssetType);
+            CreateAssetFromExistingType(existingAssetType);
+        }
+
+        private static string GetClassSafeTypeName(string rawTypeName)
+        {
+            return rawTypeName
+                .Replace('.', '_')
+                .Replace('`', '_');
+        }
+
+        private static string GetTypeNameWithoutAssembly(string fullTypeName)
+            => fullTypeName.Split('[')[0];
+
+        [CanBeNull]
+        private static Type GetEmptyTypeDerivedFrom(Type parentType)
+        {
+            TypeCache.TypeCollection foundTypes = TypeCache.GetTypesDerivedFrom(parentType);
+
+            if (foundTypes.Count == 0)
+                return null;
+
+            // Why would there be another empty type derived from GenericScriptableObject?
+            Assert.IsTrue(foundTypes.Count == 1);
+
+            Type matchingType = foundTypes.FirstOrDefault(type => type.IsEmpty());
+            return matchingType;
+        }
+
+        /// <summary>
+        /// The method generates a SHA1 hash for the generic parameters and strips the length to 10 characters. If it
+        /// finds a collision, it increases the string length by one char and continues to do so until no collisions
+        /// are found.
+        /// </summary>
+        /// <returns></returns>
+        private string GetUniqueClassName()
+        {
+            string input = string.Join("_", _paramTypeNamesWithoutAssembly);
+            string hashString = Hash.SHA1(input);
+
+            int suffixLength = 10;
+            string suffix = hashString.Substring(0, suffixLength);
+
+            string GetClassName() => $"{_genericTypeClassSafeName}_{suffix}";
+
+            while (AssetDatabase.FindAssets(GetClassName()).Length != 0)
+            {
+                suffix = hashString.Substring(0, ++suffixLength);
+            }
+
+            return GetClassName();
+        }
+
+        private void CreateAssetFromExistingType(Type assetType)
+        {
+            GenericSODatabase.Add(_genericType, _paramTypes, assetType);
+            CreateAssetInteractively();
+        }
+
+        private void CreateScript(string className)
+        {
+            string fullAssetPath = $@"\\?\{Application.dataPath}/{_scriptsPath}/{className}.cs";
+            string scriptContent = GetScriptContent(className);
+
+            AssetDatabaseHelper.MakeSureFolderExists(_scriptsPath);
+            File.WriteAllText(fullAssetPath, scriptContent);
+            AssetDatabase.Refresh();
+        }
+
+        private string GetScriptContent(string className)
+        {
+            string genericTypeNameWithoutParam = _genericType.Name.Split('`')[0];
+            string paramTypeNames = string.Join(", ", _paramTypeNamesWithoutAssembly);
+
+            return $"namespace {_namespaceName} {{ public class {className} : " +
+                   $"{_genericType.Namespace}.{genericTypeNameWithoutParam}<{paramTypeNames}> {{ }} }}";
+        }
+
+        private void CreateAssetInteractively()
+        {
+            var asset = GenericScriptableObject.CreateInstance(_genericType, _paramTypes);
+            Assert.IsNotNull(asset);
+            AssetCreator.Create(asset, DefaultAssetName);
+        }
+    }
+}
--- Editor/GenericSOCreator.cs
+++ Editor/GenericSOCreator.cs
@@ -1,54 +1,22 @@
 ﻿namespace GenericScriptableObjects.Editor
 {
     using System;
-    using System.IO;
     using System.Linq;
-    using JetBrains.Annotations;
-    using SolidUtilities.Editor.EditorWindows;
-    using SolidUtilities.Editor.Helpers;
     using SolidUtilities.Extensions;
     using SolidUtilities.Helpers;
     using TypeSelectionWindows;
-    using UnityEditor;
     using UnityEditor.Callbacks;
     using UnityEngine;
-    using UnityEngine.Assertions;
 
     /// <summary>
     /// Inherit from this class and use the <see cref="CreateAsset"/> method to create an AssetCreate menu.
     /// </summary>
-    public class GenericSOCreator
+    public static class GenericSOCreator
     {
         public const string AssetCreatePath = "Assets/Create/";
 
         private const string DefaultNamespaceName = "GenericScriptableObjectsTypes";
         private const string DefaultScriptsPath = "Scripts/GenericScriptableObjectTypes";
-
-        private readonly Type _genericType;
-        private readonly Type[] _paramTypes;
-        private readonly string[] _paramTypeNamesWithoutAssembly;
-        private readonly string _className;
-        private readonly string _defaultAssetName;
-        private readonly string _scriptsPath;
-        private readonly string _namespaceName;
-
-        private GenericSOCreator(Type genericType, Type[] paramTypes, string namespaceName, string scriptsPath)
-        {
-            _genericType = genericType;
-            _paramTypes = paramTypes;
-            _scriptsPath = scriptsPath;
-            _namespaceName = namespaceName;
-
-            _paramTypeNamesWithoutAssembly = _paramTypes
-                .Select(type => GetTypeNameWithoutAssembly(type.FullName)).ToArray();
-
-            string genericTypeClassSafeName = GetClassSafeTypeName(_genericType.Name);
-            string paramTypesClassSafeNames =
-                string.Join("_", _paramTypeNamesWithoutAssembly.Select(GetClassSafeTypeName));
-
-            _className = $"{genericTypeClassSafeName}_{paramTypesClassSafeNames}";
-            _defaultAssetName = $"New {genericTypeClassSafeName}.asset";
-        }
 
         /// <summary>
         /// Creates a <see cref="GenericScriptableObject"/> asset when used in a method with the
@@ -74,7 +42,7 @@
 
             TypeSelectionWindow.Create(constraints, paramTypes =>
             {
-                var creator = new GenericSOCreator(genericType, paramTypes, namespaceName, scriptsPath);
+                var creator = new AssetCreatorHelper(genericType, paramTypes, namespaceName, scriptsPath);
                 creator.CreateAsset();
             });
         }
@@ -90,7 +58,7 @@
                 Type genericTypeWithoutArgs = AssetCreatorPersistentStorage.GenericType.Type.GetGenericTypeDefinition();
                 var paramTypes = AssetCreatorPersistentStorage.GenericType.Type.GenericTypeArguments;
 
-                var creator = new GenericSOCreator(
+                var creator = new AssetCreatorHelper(
                     genericTypeWithoutArgs,
                     paramTypes,
                     AssetCreatorPersistentStorage.NamespaceName,
@@ -122,93 +90,6 @@
             Debug.LogError($"The provided path '{scriptsPath}' is not a valid Unity path. Restricted characters are /?<>\\:*|\"");
             scriptsPath = DefaultScriptsPath;
             return scriptsPath;
-        }
-
-        [CanBeNull]
-        private static Type GetEmptyTypeDerivedFrom(Type parentType)
-        {
-            TypeCache.TypeCollection foundTypes = TypeCache.GetTypesDerivedFrom(parentType);
-
-            if (foundTypes.Count == 0)
-                return null;
-
-            // Why would there be another empty type derived from GenericScriptableObject?
-            Assert.IsTrue(foundTypes.Count == 1);
-
-            Type matchingType = foundTypes.FirstOrDefault(type => type.IsEmpty());
-            return matchingType;
-        }
-
-        private static string GetClassSafeTypeName(string rawTypeName)
-        {
-            return rawTypeName
-                .Replace('.', '_')
-                .Replace('`', '_');
-        }
-
-        private static string GetTypeNameWithoutAssembly(string fullTypeName)
-            => fullTypeName.Split('[')[0];
-
-        private void CreateAsset()
-        {
-            if (GenericSODatabase.ContainsKey(_genericType, _paramTypes))
-            {
-                CreateAssetInteractively();
-                return;
-            }
-
-            Type genericTypeWithArgs = _genericType.MakeGenericType(_paramTypes);
-
-            Type existingAssetType = GetEmptyTypeDerivedFrom(genericTypeWithArgs);
-
-            if (existingAssetType != null)
-            {
-                CreateAssetFromExistingType(existingAssetType);
-                return;
-            }
-
-            AssetCreatorPersistentStorage.SaveForAssemblyReload(genericTypeWithArgs, _namespaceName, _scriptsPath);
-            CreateScript();
-        }
-
-        private void CreateAssetFromExistingType(Type assetType)
-        {
-            GenericSODatabase.Add(_genericType, _paramTypes, assetType);
-            CreateAssetInteractively();
-        }
-
-        private void CreateAssetFromExistingType()
-        {
-            Type genericTypeWithArgs = _genericType.MakeGenericType(_paramTypes);
-            Type existingAssetType = GetEmptyTypeDerivedFrom(genericTypeWithArgs);
-            Assert.IsNotNull(existingAssetType);
-            CreateAssetFromExistingType(existingAssetType);
-        }
-
-        private void CreateScript()
-        {
-            string fullAssetPath = $@"\\?\{Application.dataPath}/{_scriptsPath}/{_className}.cs";
-            string scriptContent = GetScriptContent();
-
-            AssetDatabaseHelper.MakeSureFolderExists(_scriptsPath);
-            File.WriteAllText(fullAssetPath, scriptContent);
-            AssetDatabase.Refresh();
-        }
-
-        private string GetScriptContent()
-        {
-            string genericTypeNameWithoutParam = _genericType.Name.Split('`')[0];
-            string paramTypeNames = string.Join(", ", _paramTypeNamesWithoutAssembly);
-
-            return $"namespace {_namespaceName} {{ public class {_className} : " +
-                   $"{_genericType.Namespace}.{genericTypeNameWithoutParam}<{paramTypeNames}> {{ }} }}";
-        }
-
-        private void CreateAssetInteractively()
-        {
-            var asset = GenericScriptableObject.CreateInstance(_genericType, _paramTypes);
-            Assert.IsNotNull(asset);
-            AssetCreator.Create(asset, _defaultAssetName);
         }
     }
 }
