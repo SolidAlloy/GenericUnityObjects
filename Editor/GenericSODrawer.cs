f4614777670a34a560a4298311be8b4f7537f11a
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-29 17:49:26 +0200

    Added GenericSODrawer to fix the bug with object fields

---
+++ Editor/GenericSODrawer.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericScriptableObjects.Editor
+{
+    using System;
+    using SolidUtilities.Editor.Extensions;
+    using SolidUtilities.Editor.Helpers;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// The default object field drawn for types derived from <see cref="GenericScriptableObject"/> does not list
+    /// available assets in the object picker window. This custom property drawer looks the same but lists the
+    /// available assets.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(GenericScriptableObject), true)]
+    public class GenericSODrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorDrawHelper.InPropertyWrapper(position, label, property, () =>
+            {
+                Rect posWithoutLabel = EditorGUI.PrefixLabel(position, label);
+                Type filterType = GetFilterType(property);
+                property.objectReferenceValue = EditorGUI.ObjectField(
+                    posWithoutLabel, property.objectReferenceValue, filterType, false);
+            });
+        }
+
+        private static Type GetFilterType(SerializedProperty property)
+        {
+            Type genericType = property.GetObjectType();
+            GenericSODatabase.TryGetValue(genericType, out Type concreteType);
+            return concreteType ?? genericType;
+        }
+    }
+}
