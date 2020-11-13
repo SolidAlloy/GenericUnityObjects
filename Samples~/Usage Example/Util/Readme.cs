3a3f2f925ae7652e9acbaac9405a5568b833b63e
SolidAlloy <artem.perepelytsia@4a-games.com.mt>
2020-11-13 13:56:30 +0200

    refactor: Removed custom editor and started using ResizableTextArea attribute for the Readme asset

--- Samples~/Usage Example/Util/Readme.cs
+++ Samples~/Usage Example/Util/Readme.cs
@@ -1,56 +1,12 @@
 ﻿namespace GenericScriptableObjects.Usage_Example.Util
 {
-#if UNITY_EDITOR
-    using SolidUtilities.Editor.Helpers;
-    using UnityEditor;
-#endif
+    using JetBrains.Annotations;
+    using SolidUtilities.Attributes;
     using UnityEngine;
 
     internal class Readme : ScriptableObject
     {
-        [SerializeField]
-        internal string Description = null;
+        [SerializeField, ResizableTextArea, UsedImplicitly]
+        private string _description = null;
     }
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(Readme))]
-    internal class ReadmeEditor : Editor
-    {
-        private SerializedProperty _description;
-        private GUIStyle _style;
-
-        private GUIStyle Style
-        {
-            get
-            {
-                if (_style == null)
-                    _style = new GUIStyle(EditorStyles.textField) { wordWrap = true };
-
-                return _style;
-            }
-        }
-
-        private string Description
-        {
-            get => _description.stringValue;
-            set => _description.stringValue = value;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            string textAreaValue = null;
-
-            if (EditorDrawHelper.CheckIfChanged(() => textAreaValue = EditorGUILayout.TextArea(Description, Style)))
-            {
-                Description = textAreaValue;
-                serializedObject.ApplyModifiedProperties();
-            }
-        }
-
-        private void OnEnable()
-        {
-            _description = serializedObject.FindProperty(nameof(Readme.Description));
-        }
-    }
-#endif
 }
