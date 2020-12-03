0d0a24ba27d2ef1b02fe24e21ab6e82cd9fbb146
SolidAlloy <perepelartem@gmail.com>
2020-12-03 18:18:06 +0200

    fix: Disable the warning that appeared after the usage sample is imported

--- Samples~/Usage Example/Util/Readme.cs
+++ Samples~/Usage Example/Util/Readme.cs
@@ -6,7 +6,9 @@
 
     internal class Readme : ScriptableObject
     {
+#pragma warning disable 414
         [SerializeField, ResizableTextArea, UsedImplicitly]
         private string _description = null;
+#pragma warning restore 414
     }
 }
