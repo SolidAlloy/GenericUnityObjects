a4b750591d582770a393c5fc8c8cd5f38429081c
SolidAlloy <perepelartem@gmail.com>
2020-12-13 23:18:43 +0200

    feat: Removed GenericScriptableObject.CreateInstance(Type genericTypeWithoutTypeParams, params Type[] paramTypes)
    The generic type can be easily assembled outside of the method with help of genericTypeDefinition.MakeGenericType(paramTypes). There is no need in this specific method overload.

--- Runtime/GenericScriptableObject.cs
+++ Runtime/GenericScriptableObject.cs
@@ -42,37 +42,5 @@
         {
             return (T) CreateInstance(typeof(T));
         }
-
-        /// <summary>
-        /// Creates an instance of <see cref="GenericScriptableObject"/> using a generic type definition
-        /// (generic type without generic argument types) and an array of generic argument types.
-        /// </summary>
-        /// <param name="genericTypeWithoutTypeParams">
-        /// Generic type derived from <see cref="GenericScriptableObject"/> without generic argument types (e.g. Generic&lt;,>).
-        /// </param>
-        /// <param name="paramTypes">Types of the generic arguments.</param>
-        /// <returns>
-        /// Instance of <paramref name="genericTypeWithoutTypeParams"/> with param types <paramref name="paramTypes"/>>.
-        /// </returns>
-        /// <exception cref="ArgumentException">
-        /// If the number of generic argument types passed into the method is not equal to the number of generic
-        /// arguments the type can take.
-        /// </exception>
-        [PublicAPI, CanBeNull, Pure]
-        public static GenericScriptableObject CreateInstance(Type genericTypeWithoutTypeParams, params Type[] paramTypes)
-        {
-            int genericArgCount = genericTypeWithoutTypeParams.GetGenericArguments().Length;
-            int paramTypesCount = paramTypes.Length;
-
-            if (genericArgCount != paramTypesCount)
-            {
-                throw new ArgumentException(
-                    $"Number of generic argument types ({paramTypesCount}) is not equal to the number of " +
-                    $"generic arguments the type can take ({genericArgCount}).", nameof(genericTypeWithoutTypeParams));
-            }
-
-            Type genericType = genericTypeWithoutTypeParams.MakeGenericType(paramTypes);
-            return (GenericScriptableObject) CreateInstance(genericType);
-        }
     }
 }
