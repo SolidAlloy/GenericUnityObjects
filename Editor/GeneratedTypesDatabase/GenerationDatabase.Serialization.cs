0c3bcbe919a1f97382faf5d2faa14035d0a8893a
SolidAlloy <perepelartem@gmail.com>
2021-01-14 17:46:33 +0200

    Included using statement for FastIterationDictionary

--- Editor/GeneratedTypesDatabase/GenerationDatabase.Serialization.cs
+++ Editor/GeneratedTypesDatabase/GenerationDatabase.Serialization.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Runtime.Serialization;
+    using SolidUtilities.Helpers;
     using UnityEditor;
     using UnityEngine;
     using Object = UnityEngine.Object;
--- Editor/GeneratedTypesDatabase/GenerationDatabase.cs
+++ Editor/GeneratedTypesDatabase/GenerationDatabase.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using GenericUnityObjects.Util;
+    using SolidUtilities.Helpers;
     using UnityEditor;
     using UnityEngine;
     using Util;
