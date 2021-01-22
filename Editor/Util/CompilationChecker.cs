df6c68c3c634cf916653bdf808f2290643bd2124
SolidAlloy <perepelartem@gmail.com>
2021-01-22 17:42:57 +0200

    Added CompilationChecker
    It logs how many assemblies have been reloaded and how much time it took. Useful to know to make sure that only the affected assemblies are reloaded.

---
+++ Editor/Util/CompilationChecker.cs
@@ -0,0 +1,60 @@
+﻿namespace Testing.Editor
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using GenericUnityObjects.Util;
+    using UnityEditor;
+    using UnityEditor.Compilation;
+    using Debug = UnityEngine.Debug;
+
+    [InitializeOnLoad]
+    internal static class CompilationChecker
+    {
+        private static readonly Dictionary<string, Stopwatch> Dictionary;
+        private static readonly Stopwatch Stopwatch;
+
+        static CompilationChecker()
+        {
+#if GENERIC_UNITY_OBJECTS_DEBUG
+            CompilationPipeline.compilationStarted += OnCompilationStarted;
+            CompilationPipeline.compilationFinished += OnCompilationFinished;
+            CompilationPipeline.assemblyCompilationStarted += OnAssemblyCompilationStarted;
+            CompilationPipeline.assemblyCompilationFinished += OnAssemblyCompilationFinished;
+            Dictionary = new Dictionary<string, Stopwatch>();
+            Stopwatch = new Stopwatch();
+#endif
+        }
+
+        private static void OnCompilationStarted(object context)
+        {
+            Dictionary.Clear();
+            Stopwatch.Start();
+        }
+
+        private static void OnCompilationFinished(object context)
+        {
+            var elapsed = Stopwatch.Elapsed;
+
+            Stopwatch.Stop();
+            Stopwatch.Reset();
+
+            foreach (var pair in Dictionary)
+            {
+                Debug.Log($"Assembly {pair.Key.Replace("Library/ScriptAssemblies/", string.Empty)} " +
+                          $"built in {pair.Value.Elapsed.TotalSeconds:F} seconds.");
+            }
+
+            Debug.Log($"Total compilation time: {elapsed.TotalSeconds:F} seconds.");
+        }
+
+        private static void OnAssemblyCompilationStarted(string value)
+        {
+            Dictionary.Add(value, Stopwatch.StartNew());
+        }
+
+        private static void OnAssemblyCompilationFinished(string value, CompilerMessage[] messages)
+        {
+            Dictionary[value].Stop();
+        }
+    }
+}
