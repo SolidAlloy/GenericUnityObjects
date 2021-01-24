4f66b46dd38b99544fc61878f5b604a8389b864f
SolidAlloy <perepelartem@gmail.com>
2021-01-24 13:26:40 +0200

    Added GenericScriptableObject Concrete Class integration tests

---
+++ Tests/Integration/GenericScriptableObject_ConcreteClass.cs
@@ -0,0 +1,178 @@
+﻿namespace GenericUnityObjects.EditorTests.Integration
+{
+    using System;
+    using System.Collections;
+    using System.IO;
+    using JetBrains.Annotations;
+    using NUnit.Framework;
+    using UnityEditor;
+    using UnityEngine.TestTools;
+    using Util;
+
+    public class GenericScriptableObject_ConcreteClass
+    {
+        private static readonly Type DefaultArgumentType = typeof(int);
+        private const string SkipDefaultAssetCreation = "SkipDefaultAssetCreation";
+        private const string NewTypeName = "NewName";
+
+
+        // UnitySetUp is required because SetUp runs each time after domain reload which is unwanted for UnityTest methods.
+        [UnitySetUp, UsedImplicitly]
+        public IEnumerator BeforeEachTest()
+        {
+            IntegrationTestHelper.CreateFolder(IntegrationTestHelper.TestingDir);
+
+            if (ShouldSkipDefaultAssetCreation())
+                yield break;
+
+            IntegrationTestHelper.AddScript(1);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            IntegrationTestHelper.OpenTestingFolder();
+            IntegrationTestHelper.TriggerAssetCreation(DefaultArgumentType);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+
+            IntegrationTestHelper.FinishInteractiveCreation();
+        }
+
+        [UnityTearDown, UsedImplicitly]
+        public IEnumerator AfterEachTest()
+        {
+            AssetDatabase.DeleteAsset(IntegrationTestHelper.TestingDir);
+            AssetDatabase.Refresh();
+            yield return new RecompileScripts(false);
+        }
+
+        [Test]
+        public void Concrete_class_dll_is_added_when_concrete_class_is_instantiated_for_the_first_time()
+        {
+            Assert.That(File.Exists($"{Config.AssembliesDirPath}/{IntegrationTestHelper.DefaultGenericClassName}1_{DefaultArgumentType.Name}.dll"));
+        }
+
+        [Test]
+        public void Assembly_does_not_reload_when_concrete_class_is_instantiated_for_the_second_time()
+        {
+            IntegrationTestHelper.TriggerAssetCreation(DefaultArgumentType);
+
+            Assert.IsFalse(EditorApplication.isCompiling);
+
+            IntegrationTestHelper.FinishInteractiveCreation();
+        }
+
+        [Test]
+        public void Asset_is_created_with_the_default_name_when_CreateGenericAssetMenu_is_empty()
+        {
+            Assert.That(File.Exists(IntegrationTestHelper.DefaultAssetPath));
+        }
+
+        [UnityTest, Category(SkipDefaultAssetCreation)]
+        public IEnumerator Asset_is_created_with_the_custom_name_when_FileName_is_specified_in_the_attribute()
+        {
+            const string expectedCustomName = "Custom Name";
+
+            IntegrationTestHelper.AddScript(1, true, $"FileName = \"{expectedCustomName}\"");
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            IntegrationTestHelper.OpenTestingFolder();
+            IntegrationTestHelper.TriggerAssetCreation(DefaultArgumentType);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+
+            IntegrationTestHelper.FinishInteractiveCreation();
+
+            Assert.That(File.Exists($"{IntegrationTestHelper.TestingDir}/{expectedCustomName}.asset"));
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_is_updated_when_type_name_changes_and_guid_is_same()
+        {
+            IntegrationTestHelper.ChangeTypeAndFileName($"{IntegrationTestHelper.DefaultGenericClassName}1", NewTypeName);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            Type genericType = IntegrationTestHelper.GetTestType($"{NewTypeName}`1");
+            IntegrationTestHelper.AssertThatConcreteClassIsUpdated(genericType, DefaultArgumentType);
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_is_updated_when_guid_changes_but_type_name_is_same()
+        {
+            IntegrationTestHelper.ChangeGUID(1);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+
+            Type genericType = IntegrationTestHelper.GetTestType($"{IntegrationTestHelper.DefaultGenericClassName}1`1");
+            IntegrationTestHelper.AssertThatConcreteClassIsUpdated(genericType, DefaultArgumentType);
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_is_updated_when_type_name_changes_but_script_name_and_guid_does_not()
+        {
+            IntegrationTestHelper.ChangeTypeNameOnly(1, NewTypeName);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            Type genericType = IntegrationTestHelper.GetTestType($"{IntegrationTestHelper.DefaultGenericClassName}1`1");
+            IntegrationTestHelper.AssertThatConcreteClassIsUpdated(genericType, DefaultArgumentType);
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_is_updated_when_generic_argument_name_changes()
+        {
+            const string newArgName = "TTest";
+
+            IntegrationTestHelper.ChangeArgumentName(1, newArgName);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            Type genericType = IntegrationTestHelper.GetTestType($"{IntegrationTestHelper.DefaultGenericClassName}1`1");
+            IntegrationTestHelper.AssertThatConcreteClassIsUpdated(genericType, DefaultArgumentType);
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_is_removed_when_type_is_removed()
+        {
+            IntegrationTestHelper.RemoveScript(1);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            IntegrationTestHelper.AssertThatConcreteClassIsRemoved(DefaultArgumentType);
+        }
+
+        [UnityTest]
+        public IEnumerator Concrete_class_changes_when_type_name_and_guid_are_different()
+        {
+            IntegrationTestHelper.ChangeTypeNameOnly(1, NewTypeName);
+            IntegrationTestHelper.ChangeGUID(1);
+
+            yield return new WaitForDomainReload();
+            yield return null;
+            yield return new WaitForDomainReload();
+
+            IntegrationTestHelper.AssertThatConcreteClassChanged(NewTypeName, DefaultArgumentType);
+        }
+
+        private static bool ShouldSkipDefaultAssetCreation()
+        {
+            return TestContext.CurrentContext.Test.Properties["Category"].Contains(SkipDefaultAssetCreation);
+        }
+    }
+}
--- Tests/Integration/IntegrationTestHelper.cs
+++ Tests/Integration/IntegrationTestHelper.cs
@@ -1,69 +1,213 @@
 ﻿namespace GenericUnityObjects.EditorTests.Integration
 {
+    using System;
     using System.IO;
+    using System.Reflection;
+    using Editor.ScriptableObjects.SelectionWindow;
+    using NUnit.Framework;
     using UnityEditor;
+    using Util;
 
     internal static class IntegrationTestHelper
     {
         public const string TestingDir = "Assets/Testing";
-        public const string DefaultClassName = "GenericSOTest";
+        public const string DefaultGenericClassName = "GenericSOTest";
+        public const string DefaultAssetPath = TestingDir + "/New " + DefaultGenericClassName + "1`1.asset";
 
-        private static string GetAssetName(int number)
+        private static string GetAssetPath(int number)
         {
-            return $"{DefaultClassName}{number}.cs";
+            return $"{TestingDir}/{DefaultGenericClassName}{number}.cs";
         }
 
         public static void AddScript(int scriptNum, bool withAttribute = true, params string[] genericAssetMenuParams)
         {
             string usingLine = "using GenericUnityObjects;\n";
             string attributeLine = $"[CreateGenericAssetMenu({string.Join(", ", genericAssetMenuParams)})]\n";
-            string classLine = $"public class {DefaultClassName}{scriptNum}<T> : GenericScriptableObject {{}}";
+            string classLine = $"public class {DefaultGenericClassName}{scriptNum}<T> : GenericScriptableObject {{}}";
 
             string content = withAttribute ? usingLine + attributeLine + classLine : usingLine + classLine;
 
-            File.WriteAllText($"{TestingDir}/{GetAssetName(scriptNum)}", content);
+            File.WriteAllText(GetAssetPath(scriptNum), content);
             AssetDatabase.Refresh();
         }
 
-        public static void ChangeTypeAndFileName(int scriptNum, string newName)
+        public static void AddArgumentScript(string argName)
         {
-            string content = File.ReadAllText($"{TestingDir}/{GetAssetName(scriptNum)}");
-            content = content.Replace(DefaultClassName, newName);
-            File.Delete($"{TestingDir}/{GetAssetName(scriptNum)}");
-            File.WriteAllText($"{TestingDir}/{newName}{scriptNum}.cs", content);
-            File.Move($"{TestingDir}/{DefaultClassName}{scriptNum}.cs.meta", $"{TestingDir}/{newName}{scriptNum}.cs.meta");
+            File.WriteAllText($"{TestingDir}/{argName}.cs", $"[System.Serializable] public class {argName} {{ }}");
+            AssetDatabase.Refresh();
+        }
+
+        public static void ChangeTypeAndFileName(string oldClassName, string newClassName)
+        {
+            string oldFilePath = $"{TestingDir}/{oldClassName}.cs";
+            string newFilePath = $"{TestingDir}/{newClassName}.cs";
+
+            string content = File.ReadAllText(oldFilePath);
+            content = content.Replace(oldClassName, newClassName);
+            File.Delete(oldFilePath);
+            File.WriteAllText(newFilePath, content);
+            File.Move($"{oldFilePath}.meta", $"{newFilePath}.meta");
             AssetDatabase.Refresh();
         }
 
         public static void ChangeGUID(int scriptNum)
         {
-            string content = File.ReadAllText($"{TestingDir}/{DefaultClassName}{scriptNum}.cs");
-            File.WriteAllText($"{TestingDir}/{DefaultClassName}{scriptNum}`1.cs", content);
-            AssetDatabase.DeleteAsset($"{TestingDir}/{DefaultClassName}{scriptNum}.cs");
+            string content = File.ReadAllText($"{TestingDir}/{DefaultGenericClassName}{scriptNum}.cs");
+            File.WriteAllText($"{TestingDir}/{DefaultGenericClassName}{scriptNum}`1.cs", content);
+            AssetDatabase.DeleteAsset($"{TestingDir}/{DefaultGenericClassName}{scriptNum}.cs");
             AssetDatabase.Refresh();
         }
 
         public static void ChangeTypeNameOnly(int scriptNum, string newName)
         {
-            string assetPath = $"{TestingDir}/{GetAssetName(scriptNum)}";
+            string assetPath = GetAssetPath(scriptNum);
             string content = File.ReadAllText(assetPath);
-            content = content.Replace(DefaultClassName, newName);
+            content = content.Replace(DefaultGenericClassName, newName);
             File.WriteAllText(assetPath, content);
             AssetDatabase.Refresh();
         }
 
         public static void ChangeArgumentName(int scriptNum, string newArgumentName)
         {
-            string content = File.ReadAllText($"{TestingDir}/{GetAssetName(scriptNum)}");
+            string content = File.ReadAllText(GetAssetPath(scriptNum));
             content = content.Replace("<T>", $"<{newArgumentName}>");
-            File.WriteAllText($"{TestingDir}/{GetAssetName(scriptNum)}", content);
+            File.WriteAllText(GetAssetPath(scriptNum), content);
             AssetDatabase.Refresh();
         }
 
         public static void RemoveScript(int scriptNum)
         {
-            AssetDatabase.DeleteAsset($"{TestingDir}/{GetAssetName(scriptNum)}");
+            AssetDatabase.DeleteAsset(GetAssetPath(scriptNum));
             AssetDatabase.Refresh();
+        }
+
+        public static bool ValidateMenuItem(string menuItem)
+        {
+            MethodInfo validateMenuItem =
+                typeof(EditorApplication).GetMethod("ValidateMenuItem", BindingFlags.Static | BindingFlags.NonPublic);
+
+            Assert.IsNotNull(validateMenuItem);
+
+            return (bool) validateMenuItem.Invoke(null, new object[] { menuItem });
+        }
+
+        public static void AssertThatConcreteClassIsUpdated(Type genericTypeDefinition, Type argType)
+        {
+            // scriptable object of new type can be instantiated
+            var genericType = genericTypeDefinition.MakeGenericType(argType);
+            var testInstance = GenericScriptableObject.CreateInstance(genericType);
+            Assert.IsNotNull(testInstance);
+
+            // LoadAssetAtPath is not null
+            var asset = AssetDatabase.LoadMainAssetAtPath(DefaultAssetPath);
+            Assert.IsNotNull(asset);
+        }
+
+        public static void AssertThatConcreteClassIsRemoved(Type argType)
+        {
+            // dll does not exist
+            Assert.IsFalse(File.Exists($"{Config.AssembliesDirPath}/{DefaultGenericClassName}1_{argType.Name}.dll"));
+
+            // LoadAssetAtPath is null
+            var asset = AssetDatabase.LoadMainAssetAtPath(DefaultAssetPath);
+            Assert.IsNull(asset);
+        }
+
+        public static void AssertThatConcreteClassChanged(string newTypeName, Type argType)
+        {
+            // old dll does not exist
+            Assert.IsFalse(File.Exists($"{Config.AssembliesDirPath}/{DefaultGenericClassName}1_{argType.Name}.dll"));
+
+            // LoadAssetAtPath is null
+            var asset = AssetDatabase.LoadMainAssetAtPath(DefaultAssetPath);
+            Assert.IsNull(asset);
+
+            // new type is added to menu items
+            Assert.IsTrue(ValidateMenuItem($"Assets/Create/{newTypeName}1<T>"));
+        }
+
+        public static void CreateFolder(string folder)
+        {
+            var pathParts = folder.Split(new[] { '/' }, 2);
+            AssetDatabase.CreateFolder(pathParts[0], pathParts[1]);
+        }
+
+        public static void OpenTestingFolder()
+        {
+            var folderObject = AssetDatabase.LoadAssetAtPath<DefaultAsset>(TestingDir);
+            Assert.IsNotNull(folderObject);
+
+            var projectBrowserType = typeof(Editor).Assembly.GetType("UnityEditor.ProjectBrowser");
+            var projectBrowser = EditorWindow.GetWindow(projectBrowserType);
+
+            // private void ShowFolderContents(int folderInstanceID, bool revealAndFrameInFolderTree)
+            MethodInfo showFolderContents = projectBrowserType.GetMethod(
+                "ShowFolderContents",
+                BindingFlags.Instance | BindingFlags.NonPublic,
+                null,
+                new[] { typeof(int), typeof(bool) },
+                null);
+
+            Assert.NotNull(showFolderContents);
+
+            showFolderContents.Invoke(projectBrowser, new object[] { folderObject.GetInstanceID(), true });
+
+            string openPath = GetCurrentlyOpenPathInProject();
+
+            Assert.AreEqual(TestingDir, openPath);
+        }
+
+        private static string GetCurrentlyOpenPathInProject()
+        {
+            MethodInfo getActiveFolderPath = typeof(ProjectWindowUtil).GetMethod(
+                "GetActiveFolderPath",
+                BindingFlags.Static | BindingFlags.NonPublic);
+
+            return (string) getActiveFolderPath.Invoke(null, null);
+        }
+
+
+        public static void TriggerAssetCreation(Type typeToSet)
+        {
+            EditorApplication.ExecuteMenuItem($"Assets/Create/{DefaultGenericClassName}1<T>");
+
+            Assert.IsTrue(EditorWindow.HasOpenInstances<OneTypeSelectionWindow>());
+
+            var selectionWindow = EditorWindow.GetWindow<OneTypeSelectionWindow>();
+
+            selectionWindow.OnTypeSelected(new[] { typeToSet });
+        }
+
+        public static void FinishInteractiveCreation()
+        {
+            Type projectBrowserType = Assembly
+                .Load("UnityEditor.CoreModule")
+                .GetType("UnityEditor.ProjectBrowser");
+
+            FieldInfo lastInstanceField = projectBrowserType.GetField(
+                "s_LastInteractedProjectBrowser",
+                BindingFlags.Static | BindingFlags.Public);
+
+            Assert.IsNotNull(lastInstanceField);
+
+            object lastInstance = lastInstanceField.GetValue(null);
+
+            Assert.IsNotNull(lastInstance, "Project window was not found in the current layout. Please add it to the editor layout.");
+
+            MethodInfo endRenaming = projectBrowserType.GetMethod(
+                "EndRenaming",
+                BindingFlags.Instance | BindingFlags.Public);
+
+            Assert.IsNotNull(endRenaming);
+
+            endRenaming.Invoke(lastInstance, null);
+        }
+
+        public static Type GetTestType(string typeName)
+        {
+            Type type = Assembly.Load("Assembly-CSharp").GetType(typeName);
+            Assert.IsNotNull(type);
+            return type;
         }
     }
 }
