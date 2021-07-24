c4e1663c410ce885ba432162f5bd4789ec5fdf34
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-24 23:29:29 +0300

    fix: Removed unnecessary Assembly-CSharp inclusion in type dropdown

--- Editor/Util/NonGenericAttribute.cs
+++ Editor/Util/NonGenericAttribute.cs
@@ -10,13 +10,10 @@
     /// </summary>
     internal class NonGenericAttribute : InheritsAttribute
     {
-        private static readonly string[] _additionalAssemblies = { "Assembly-CSharp" };
-
         public NonGenericAttribute(Type[] baseTypes)
             : base(baseTypes)
         {
             ExcludeNone = true;
-            IncludeAdditionalAssemblies = _additionalAssemblies;
             ShortName = true;
 
             // When type has inheritance constraints, there's usually only a few types available, so expanding all
